7d507e9e9ed756dbb36d5ca6607fddcd43fdc103
Cornelius <0xCornelius@protonmail.com>
2022-03-07 14:51:18 +0100

    deadline + threashold + timeLeft + withdraw + stake

--- packages/hardhat-ts/contracts/Staker.sol
+++ packages/hardhat-ts/contracts/Staker.sol
@@ -7,9 +7,33 @@ import './ExampleExternalContract.sol';
 contract Staker {
   ExampleExternalContract public exampleExternalContract;
 
-  constructor(address exampleExternalContractAddress) public {
+  event Stake(address indexed sender, uint256 amount);
+  mapping(address => uint256) public balances;
+  uint256 public constant threshold = 1 ether;
+  uint256 deadline;
+
+  constructor(address exampleExternalContractAddress) {
     exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
+    deadline = block.timestamp + 1 minutes;
   }
+
+  function timeLeft() public view returns (uint256) {
+    return deadline - block.timestamp;
+  }
+
+  function withdraw(address ad) public {
+    require(deadline < block.timestamp, 'Deadline to withdraw not reached yet');
+    uint256 amountToWithdraw = balances[ad];
+    (bool sent, ) = ad.call{value: amountToWithdraw}('');
+    require(sent, 'Failed to withdraw Ether');
+  }
+
+  function stake() public payable {
+    balances[msg.sender] = balances[msg.sender] + msg.value;
+    emit Stake(msg.sender, msg.value);
+  }
+
+  function complete() public {}
 
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
