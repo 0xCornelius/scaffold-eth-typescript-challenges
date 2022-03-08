e4a8fc4c611e05f7a502a7504c98bc6c574a2701
Cornelius <0xCornelius@protonmail.com>
2022-03-08 12:02:20 +0100

    add receive + execute + fix time left

--- packages/hardhat-ts/contracts/Staker.sol
+++ packages/hardhat-ts/contracts/Staker.sol
@@ -10,30 +10,50 @@ contract Staker {
   event Stake(address indexed sender, uint256 amount);
   mapping(address => uint256) public balances;
   uint256 public constant threshold = 1 ether;
-  uint256 deadline;
+  uint256 public deadline = block.timestamp + 30 seconds;
+  bool openForWithdraw = false;
 
   constructor(address exampleExternalContractAddress) {
     exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
-    deadline = block.timestamp + 1 minutes;
+  }
+
+  modifier notCompleted() {
+    require(!exampleExternalContract.completed(), 'External contract already completed');
+    _;
   }
 
   function timeLeft() public view returns (uint256) {
-    return deadline - block.timestamp;
+    if (block.timestamp >= deadline) {
+      return 0;
+    } else {
+      return deadline - block.timestamp;
+    }
   }
 
-  function withdraw(address ad) public {
-    require(deadline < block.timestamp, 'Deadline to withdraw not reached yet');
-    uint256 amountToWithdraw = balances[ad];
+  function withdraw(address ad) public notCompleted {
+    require(openForWithdraw, 'Contract not open for withdraw');
+    uint256 amountToWithdraw = balances[msg.sender];
+    balances[msg.sender] = 0;
     (bool sent, ) = ad.call{value: amountToWithdraw}('');
     require(sent, 'Failed to withdraw Ether');
   }
 
-  function stake() public payable {
+  function stake() public payable notCompleted {
     balances[msg.sender] = balances[msg.sender] + msg.value;
     emit Stake(msg.sender, msg.value);
   }
 
-  function complete() public {}
+  function execute() public notCompleted {
+    if (address(this).balance > threshold) {
+      exampleExternalContract.complete{value: address(this).balance}();
+    } else if (deadline < block.timestamp) {
+      openForWithdraw = true;
+    }
+  }
+
+  receive() external payable {
+    stake();
+  }
 
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
